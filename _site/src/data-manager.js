405f949b4a03d847c748c426acbb8c73af9e7573
DrSkelettos <saniel7693@gmx.de>
2025-03-04 21:10:20 +0100

    Hide Saving Hint When Hiding Modal

--- _site/src/data-manager.js
+++ _site/src/data-manager.js
@@ -10,12 +10,20 @@ setOriginalData();
 
 function checkData() {
     let changed = false;
+    const currentData = {
+        employees: JSON.stringify(employees),
+        therapies: JSON.stringify(therapies),
+        patients: JSON.stringify(patients),
+        poststationaer: JSON.stringify(poststationaer)
+    };
+
     for (const key in original) {
-        if (original[key] != JSON.stringify(window[key])) {
+        if (original[key] !== currentData[key]) {
             changed = true;
             break;
         }
     }
+
     if (changed)
         document.getElementById("data-changed").classList.remove("d-none");
     else
