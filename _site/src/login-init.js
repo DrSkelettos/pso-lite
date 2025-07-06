eb6a8664177c635812d0adaa0b2bc2ed8591989f
DrSkelettos <saniel7693@gmx.de>
2025-07-06 15:31:06 +0200

    Misc

---
+++ _site/src/login-init.js
@@ -0,0 +1,7 @@
+// Initialize the login page
+function initLoginPage() {
+    init('Login');
+}
+
+// Add event listener when the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', initLoginPage);
